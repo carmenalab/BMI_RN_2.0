ba3c4f05f280608152c63eaed13af65041b2082b
Ryan Neely <ryanneely11@gmail.com>
2017-02-06 18:42:36 -0800

    Finished playback process function, but untested

--- playback.py
+++ playback.py
@@ -47,5 +47,40 @@ def read_line(string):
 ### a function to run the simulation. Meant to be run in a separate process.
 def playback(var_dict,engage_var):
 	log = open(var_dict["log_file"],'r')
-	line = log.readline()
-	
+	##initalize the feedback value
+	freq = 1000
+	##start the feedback process
+	br.start_feedback(freq, var_dict['samp_int'])
+	line = log.readline()[:-1] ##get the text for this line, excluding the return char
+	while line != "" ##case where you've reached the end of the log
+		##sleep for the sample interval
+		time.sleep(var_dict['samp_int']/1000.0)
+		if line == "T1": ##case where a target 1 has been hit
+			br.send_event(var_dict['t1_event'])
+			print "T1!"
+			time.sleep(3)
+			##resume feedback from the last freq value
+			br.resume_feedback(freq)
+		elif line == "T2": ##case for target 2
+			br.send_event(var_dict['t2_event'])
+			print "T2!"
+			time.sleep(3)
+			##resume feedback from the last feedback value
+			br.resume_feedback(freq)
+		elif line == "Timeout": ##case for timeout (miss)
+			br.send_event(var_dict['miss_event'])
+			br.stop_feedback()
+			br.play_noise()
+			print "Timeout!"
+			time.sleep(var_dict['timout_pause'])
+			##resume feedback
+			br.resume_feedback(freq)
+		else: ##case where this is a cursor/frequency value set
+			##get the cursor and frequency values
+			cval,freq = read_line(line)
+			##set the frequency
+			br.set_feedback(freq)
+	br.stop_feedback()
+	print "End of log file"
+	return None
+
