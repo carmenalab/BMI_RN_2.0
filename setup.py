05534568899203d81020b87556e068202055e228
Ryan Neely <ryanneely11@gmail.com>
2017-02-17 13:51:48 -0800

    Trying to make setup compatible with windoes 10

--- setup.py
+++ setup.py
@@ -25,6 +25,17 @@ elif _platform == 'win32' and pl.release() == '7':
 	'src/SineWave.cpp','src/feedback.cpp']
 	INCLUDES = ['include']
 
+elif _platform == 'win32' and pl.release() == '10':
+	library_list = ['ksuser', 'winmm', 'Wsock32', 'kernel32',
+	'user32','gdi32','winspool','comdlg32','advapi32','shell32',
+	'ole32','oleaut32','uuid','odbc32','odbccp32', 'PlexClient', 'nidaqmxbase', 'python27']
+	libraryDirs = ['lib']
+	pre_defs = [('__LITTLE_ENDIAN__', None), ('__WINDOWS_WASAPI__', None), ('__WINDOWS_MM__', None)]
+	MAIN_SOURCES = ['src/BMI_RN.cpp','src/plex_utils.cpp', 'src/cursor.cpp', 'src/reward.cpp',
+	'src/RtAudio.cpp', 'src/Stk.cpp', 'src/RtWvOut.cpp', 'src/Mutex.cpp', 'src/Noise.cpp', 
+	'src/SineWave.cpp','src/feedback.cpp']
+	INCLUDES = ['include']
+
 
 module1 = Extension('BMI_RN', sources = MAIN_SOURCES, 
 	language = 'c++', include_dirs = INCLUDES, 
