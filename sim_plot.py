97c367b185d0a9e5bdb8716791c3b4c3f93384e7
Ryan Neely <ryanneely11@gmail.com>
2017-02-22 17:27:39 -0800

    Everything working; adjusted timeout timing to match audio playback (still not perfect but close)

--- sim_plot.py
+++ sim_plot.py
@@ -9,9 +9,10 @@ import re
 
 class SubplotAnimation(animation.TimedAnimation):
     def __init__(self,t1_val,t2_val,mid_val,log_path,
-        bin_size=100,target_pause=3,timeout_pause=10):
+        bin_size=100,target_pause=4.3,timeout_pause=11.3):
         ##set up our figure
         fig = plt.figure()
+        fig.set_size_inches(12,12)
         ax1 = fig.add_subplot(2, 1, 2)
         ax2 = fig.add_subplot(2, 2, 1)
         ax3 = fig.add_subplot(2, 2, 2)
@@ -26,19 +27,19 @@ class SubplotAnimation(animation.TimedAnimation):
         ##now get the data arrays from the log file
         self.e1,self.e2,self.cval = self.parse_log()
         self.x = np.arange(-2000/self.bin_size,0)
-        self.t = np.arange(self.e1.size)
+        self.t = np.arange(10*60*30)
         self.t1 = np.ones(self.e1.size)*t1_val
         self.t2 = np.ones(self.e2.size)*t2_val
         self.mid = np.ones(self.e1.size)*mid_val
         self.show_n = 2000/self.bin_size ##number of bins to show (here it's 2 sec worth)
 
         ##add labels, etc
-        ax1.set_ylabel('Value, A.U.',fontsize=14)
+        ax1.set_ylabel('Value, A.U.',fontsize=20)
         ax1.yaxis.set_label_position("right")
-        ax1.set_xlabel('Bins',fontsize=14)
+        ax1.set_xlabel('Bins',fontsize=20)
         ax1.set_ylim(t2_val-0.15,t1_val+0.15)
         ax1.set_xlim(-self.show_n, 2) ##this should give us 2 secs of data visible
-        ax1.set_title("Cursor value",fontsize=14,weight='bold')
+        ax1.set_title("Cursor value",fontsize=20,weight='bold')
         ##add the lines to this plot for the cursor
         self.line1 = Line2D([], [], color='black',linewidth=2) ##our cursor line
         self.line1t1 = Line2D([], [], color='red', linewidth=3,alpha=0.5,label="Rewarded target") ##the T1 line crossing
@@ -51,41 +52,41 @@ class SubplotAnimation(animation.TimedAnimation):
         ax1.set_yticks([self.t2_val,self.mid_val,self.t1_val])
         ax1.set_yticklabels(['Unewarded','Baseline','Rewarded'])
         for ticklabel in ax1.get_xticklabels():
-            ticklabel.set_fontsize(14)
+            ticklabel.set_fontsize(20)
         for ticklabel in ax1.get_yticklabels():
-            ticklabel.set_fontsize(14)
+            ticklabel.set_fontsize(20)
         #legend=ax1.legend(bbox_to_anchor=(1.2, 1))
         #legend.get_frame().set_facecolor('none')
 
         #ax1.set_aspect('equal', 'datalim')
 
         ##add the stuff for the E1 plot
-        ax2.set_ylabel('Sum of E1 spikes',fontsize=14)
-        ax2.set_xlabel('Bins',fontsize=14)
-        ax2.set_title("Ensemble 1 value",fontsize=14,weight='bold')
+        ax2.set_ylabel('Sum of E1 spikes',fontsize=20)
+        ax2.set_xlabel('Bins',fontsize=20)
+        ax2.set_title("Ensemble 1 value",fontsize=20,weight='bold')
         self.line2 = Line2D([], [], color='green',linewidth=2)
         ax2.add_line(self.line2)
         ax2.set_xlim(-self.show_n, 2)
-        ax2.set_ylim(0, np.nanmax(self.e1)+1)
+        ax2.set_ylim(0, max(np.nanmax(self.e1),np.nanmax(self.e2)))
         for ticklabel in ax2.get_xticklabels():
-            ticklabel.set_fontsize(14)
+            ticklabel.set_fontsize(20)
         for ticklabel in ax2.get_yticklabels():
-            ticklabel.set_fontsize(14)
+            ticklabel.set_fontsize(20)
 
         ##add the stuff for the E2 plot
-        ax3.set_xlabel('Bins',fontsize=14)
-        ax3.set_ylabel('Sum of E2 spikes',fontsize=14)
-        ax3.set_title("Ensemble 2 value",fontsize=14,weight='bold')
+        ax3.set_xlabel('Bins',fontsize=20)
+        ax3.set_ylabel('Sum of E2 spikes',fontsize=20)
+        ax3.set_title("Ensemble 2 value",fontsize=20,weight='bold')
         self.line3 = Line2D([], [], color='blue',linewidth=2)
         ax3.add_line(self.line3)
         ax3.set_xlim(-self.show_n, 2)
-        ax3.set_ylim(0, np.nanmax(self.e2)+1)
+        ax3.set_ylim(0, max(np.nanmax(self.e2),np.nanmax(self.e1)))
         ax3.yaxis.tick_right()
         ax3.yaxis.set_label_position("right")
         for ticklabel in ax3.get_xticklabels():
-            ticklabel.set_fontsize(14)
+            ticklabel.set_fontsize(20)
         for ticklabel in ax3.get_yticklabels():
-            ticklabel.set_fontsize(14)
+            ticklabel.set_fontsize(20)
         #fig.set_size_inches(10,10)
         plt.tight_layout()
 
@@ -136,8 +137,8 @@ class SubplotAnimation(animation.TimedAnimation):
     ##T1, T2, and timeouts
     def parse_log(self):
         ##compute some vars to save space later
-        target_pause_bins = (self.target_pause*1000)/self.bin_size
-        timeout_pause_bins = (self.timeout_pause*1000)/self.bin_size
+        target_pause_bins = int(self.target_pause*1000)/self.bin_size
+        timeout_pause_bins = int(self.timeout_pause*1000)/self.bin_size
         ##init some lists to store the data;
         ##we don't really know how long this log will be
         ##so its hard to pre-allocate
@@ -170,7 +171,7 @@ class SubplotAnimation(animation.TimedAnimation):
         cval = np.asarray(cval)
         return e1, e2, cval
 
-ani = SubplotAnimation(1.2,-1.2,0,r"C:\Users\Ryan\Desktop\log.txt")
-ani.save(r"C:\Users\Ryan\Desktop\test_ani.mp4")
+ani = SubplotAnimation(3.09798,-0.46962,1.3,r"C:\Users\Ryan\Desktop\BMI_D08.txt")
+#ani.save(r"C:\Users\Ryan\Desktop\BMI_ani.mp4")
 plt.show()
 
